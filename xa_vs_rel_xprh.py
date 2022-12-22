c2e666a680c58b78c94d46f5b2b28e96b85a3d7f
Luca Cinnirella <luca.cinnirella@tutanota.com>
2022-12-22 07:10:52 +0100

    Final???

--- xa_vs_rel_xprh.py
+++ xa_vs_rel_xprh.py
@@ -20,20 +20,20 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 
 import sig_proc as sgpr
-import manual_calibrators as manc
+import calibrators as cal
+
+
+# GLOBAL VARIABLES
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Plot sizes
+WID = 5.512
+HIG = 2*WID / 1.618
 
 
 # OPTIONS HANDLER
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 parser = argparse.ArgumentParser(description='Plot time traces of edge ' +
                                  'temperature profile')
-parser.add_argument('-w', '--time_window', metavar='TIME_WINDOW', type=float,
-                    default=50.,
-                    help='Set the time window (in ms) length on which to ' +
-                    'apply median filter.\nThe step size of the median ' +
-                    'filter is also set to 1/3 of the given window length. ' +
-                    'In this way  a decimation is also applied together ' +
-                    'with the filter (default is 50)')
 parser.add_argument('-m', '--mag_equ_diag', type=str, default='EQH',
                     choices=['FPP', 'EQI', 'EQH'],
                     help='Select which diagnostic is used for magnetic ' +
@@ -47,7 +47,6 @@ elif args.mag_equ_diag == 'EQI':
 elif args.mag_equ_diag == 'EQH':
     FPG_DIAG = 'GQH'
 
-time_window = args.time_window / 1000
 mag_equ_diag = args.mag_equ_diag
 
 
@@ -55,13 +54,14 @@ mag_equ_diag = args.mag_equ_diag
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # General settings
 style.use('bmh')
-plt.rc('font', size=16)
+plt.figure(figsize=(WID, HIG))
+plt.rc('font', size=10)
+plt.rc('axes', titlesize=10)
+plt.rc('axes', labelsize=12)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+plt.rc('legend', fontsize=10)
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-px = 1/plt.rcParams['figure.dpi']  # from pixel to inches
-fig = plt.figure(figsize=(1600*px, 1000*px))
-plt.suptitle('Correlation of X-point radiator access parameter\nand X-point ' +
-             'radiator height during XPR cold core phase', fontsize=32,
-             fontweight='bold')
 
 # Subplots
 ax1 = plt.subplot2grid((1, 1), (0, 0), rowspan=1, colspan=1)
@@ -70,7 +70,7 @@ ax1.set_xlabel('$X_A$')
 ax1.set_ylabel('1 - $\\rho_{pol}$')
 
 
-for shot in [40365, 40366, 41157, 41158]:
+for shot in [40365, 40366, 41158]:
     # COLD CORE XPR TIME BOUNDARIES
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     df_cld_time = pd.read_csv('./csvs/XPR_cld_times.csv', index_col=0).loc[shot]
@@ -90,13 +90,11 @@ for shot in [40365, 40366, 41157, 41158]:
     # READING XPR POSITION DATA
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     df_pos_xpr = pd.read_csv('./csvs/XPR_position/' + str(shot) +
-                             '_XPR_position_gaussian.csv', index_col=0)
+                             '_XPR_position.csv', index_col=0)
     time_xpr = df_pos_xpr.index.values
-    R_xpr = df_pos_xpr['R (m)'].values
-    z_xpr = df_pos_xpr['z (m)'].values
     rho_xpr = df_pos_xpr['rho pol'].values
-    good_pos_xpr = df_pos_xpr['detection with scipy.signal.find_peaks ' +
-                              '(bool)'].values
+    rho_xpr_l = df_pos_xpr['rho pol lower limit'].values
+    rho_xpr_u = df_pos_xpr['rho pol upper limit'].values
 
 
     # QUERYING SHOTS
@@ -106,9 +104,9 @@ for shot in [40365, 40366, 41157, 41158]:
     fpg = sf.SFREAD(shot, FPG_DIAG)
     equ = sf.EQU(shot, diag=mag_equ_diag)
     ida = sf.SFREAD(shot, 'IDA')
-    ioa = manc.SFIOCF01(shot, sensitivity=3.)
-    iob = manc.SFIOCF01(shot)
-    ioc = manc.SFIOCF01(shot, sensitivity=2.)
+    ioc = cal.SFIOCF01(shot)
+    ioc_low = cal.SFIOCF01(shot, sensitivity=2.86)
+    ioc_upp = cal.SFIOCF01(shot, sensitivity=2.17)
     tot = sf.SFREAD(shot, 'TOT')
     bpt = sf.SFREAD(shot, 'BPT', exp='DAVIDP')
 
@@ -131,29 +129,47 @@ for shot in [40365, 40366, 41157, 41158]:
         sys.exit('Error while loading ' + FPG_DIAG)
 
     if ida.status:
-        T_u = ida.getobject('Te', cal=True).astype(np.double)
-        n_u = ida.getobject('ne', cal=True).astype(np.double)
+        T_u_matrix = ida.getobject('Te', cal=True).astype(np.double)
+        n_u_matrix = ida.getobject('ne', cal=True).astype(np.double)
         area_ida = ida.getareabase('Te')
         time_ida = ida.gettimebase('Te')
         # Choosing separatrix data
         ida_idx = sgpr.find_nearest_index(area_ida, 1.0, axis=0)
-        T_u = T_u[ida_idx, np.arange(time_ida.size)]
-        n_u = n_u[ida_idx, np.arange(time_ida.size)]
+        T_u = T_u_matrix[ida_idx, np.arange(time_ida.size)]
+        T_u_low = T_u_matrix[ida_idx + 2, np.arange(time_ida.size)]
+        T_u_upp = T_u_matrix[ida_idx - 1, np.arange(time_ida.size)]
+        n_u = n_u_matrix[ida_idx, np.arange(time_ida.size)]
+        n_u_low = n_u_matrix[ida_idx + 2, np.arange(time_ida.size)]
+        n_u_upp = n_u_matrix[ida_idx - 1, np.arange(time_ida.size)]
     else:
         sys.exit('Error while loading IDA')
 
-    if ioa.status and iob.status and ioc.status:
-        n_0_l = ioa.getobject('F01').astype(np.double)
-        n_0 = iob.getobject('F01').astype(np.double)
-        n_0_u = ioc.getobject('F01').astype(np.double)
-        time_iob = iob.gettimebase()
-        iob_par = iob.getparset('CONVERT')
-        iob_2_n = iob_par['density']
-        n_0_l = n_0_l * iob_2_n
-        n_0 = n_0 * iob_2_n
-        n_0_u = n_0_u * iob_2_n
+    if ioc.status and ioc_low.status and ioc_upp.status:
+        n_0 = ioc.getobject('F01').astype(np.double)
+        n_0_low = ioc_low.getobject('F_lower').astype(np.double)
+        n_0_upp = ioc_upp.getobject('F_upper').astype(np.double)
+        time_ioc = ioc.gettimebase()
+        ioc_par = ioc.getparset('CONVERT')
+        ioc_2_n = ioc_par['density']
+        n_0 = n_0 * ioc_2_n
+        n_0_low = n_0_low * ioc_2_n
+        n_0_upp = n_0_upp * ioc_2_n
     else:
-        sys.exit('Error while loading IOB')
+        sys.exit('Error while loading IOC')
+
+    if tot.status:
+        P_tot = tot.getobject('P_TOT', cal=True).astype(np.double)
+        time_tot = tot.gettimebase('P_TOT')
+    else:
+        sys.exit('Error while loading TOT')
+
+    if bpt.status:
+        P_rad = bpt.getobject('Pr_sepX', cal=True).astype(np.double)
+        P_rad_low = bpt.getobject('Pr_sepX-', cal=True).astype(np.double)
+        P_rad_upp = bpt.getobject('Pr_sepX+', cal=True).astype(np.double)
+        time_bpt = bpt.gettimebase('Pr_sepX')
+    else:
+        sys.exit('Error while loading BPT')
 
     print('\n\n')
     print('Querying: done')
@@ -162,8 +178,10 @@ for shot in [40365, 40366, 41157, 41158]:
     # UNIVERSAL TIME SELECTION
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Get maximum common time interval boundaries
-    start = max(time_fpg[0], time_ida[0], time_iob[0], time_xpr[0], shot_start)
-    end = min(time_fpg[-1], time_ida[-1], time_iob[-1], time_xpr[-1], shot_end)
+    start = max(time_fpg[0], time_ida[0], time_ioc[0], time_tot[0],
+                time_bpt[0], time_xpr[0], shot_start)
+    end = min(time_fpg[-1], time_ida[-1], time_ioc[-1], time_tot[-1],
+              time_bpt[-1], time_xpr[-1], shot_end)
 
     # Get indexes of boundaries for XPR
     start_index_xpr = sgpr.find_nearest_index(time_xpr, start)
@@ -177,15 +195,22 @@ for shot in [40365, 40366, 41157, 41158]:
     start_index_ida = sgpr.find_nearest_index(time_ida, start)
     end_index_ida = sgpr.find_nearest_index(time_ida, end)
 
-    # Get indexes of boundaries for IOB
-    start_index_iob = sgpr.find_nearest_index(time_iob, start)
-    end_index_iob = sgpr.find_nearest_index(time_iob, end)
+    # Get indexes of boundaries for IOC
+    start_index_ioc = sgpr.find_nearest_index(time_ioc, start)
+    end_index_ioc = sgpr.find_nearest_index(time_ioc, end)
+
+    # Get indexes of boundaries for TOT
+    start_index_tot = sgpr.find_nearest_index(time_tot, start)
+    end_index_tot = sgpr.find_nearest_index(time_tot, end)
+
+    # Get indexes of boundaries for BPT
+    start_index_bpt = sgpr.find_nearest_index(time_bpt, start)
+    end_index_bpt = sgpr.find_nearest_index(time_bpt, end)
 
     # Slice XPR quantities and relative time
-    R_xpr = R_xpr[..., start_index_xpr:end_index_xpr + 1]
-    z_xpr = z_xpr[..., start_index_xpr:end_index_xpr + 1]
     rho_xpr = rho_xpr[..., start_index_xpr:end_index_xpr + 1]
-    good_pos_xpr = good_pos_xpr[..., start_index_xpr:end_index_xpr + 1]
+    rho_xpr_l = rho_xpr_l[..., start_index_xpr:end_index_xpr + 1]
+    rho_xpr_u = rho_xpr_u[..., start_index_xpr:end_index_xpr + 1]
     time_xpr = time_xpr[..., start_index_xpr:end_index_xpr + 1]
 
     # Slice FPG quantities and relative time
@@ -199,21 +224,35 @@ for shot in [40365, 40366, 41157, 41158]:
 
     # Slice IDA quantities and relative time
     T_u = T_u[..., start_index_ida:end_index_ida + 1]
+    T_u_low = T_u_low[..., start_index_ida:end_index_ida + 1]
+    T_u_upp = T_u_upp[..., start_index_ida:end_index_ida + 1]
     n_u = n_u[..., start_index_ida:end_index_ida + 1]
+    n_u_low = n_u_low[..., start_index_ida:end_index_ida + 1]
+    n_u_upp = n_u_upp[..., start_index_ida:end_index_ida + 1]
     time_ida = time_ida[..., start_index_ida:end_index_ida + 1]
 
     # Slice IOB quantities and relative time
-    n_0_l = n_0_l[..., start_index_iob:end_index_iob + 1]
-    n_0 = n_0[..., start_index_iob:end_index_iob + 1]
-    n_0_u = n_0_u[..., start_index_iob:end_index_iob + 1]
-    time_iob = time_iob[..., start_index_iob:end_index_iob + 1]
+    n_0 = n_0[..., start_index_ioc:end_index_ioc + 1]
+    n_0_low = n_0_low[..., start_index_ioc:end_index_ioc + 1]
+    n_0_upp = n_0_upp[..., start_index_ioc:end_index_ioc + 1]
+    time_ioc = time_ioc[..., start_index_ioc:end_index_ioc + 1]
+
+    # Slice TOT quantities and relative time
+    P_tot = P_tot[..., start_index_tot:end_index_tot + 1]
+    time_tot = time_tot[..., start_index_tot:end_index_tot + 1]
+
+    # Slice BPT quantities and relative time
+    P_rad = P_rad[..., start_index_bpt:end_index_bpt + 1]
+    P_rad_low = P_rad_low[..., start_index_bpt:end_index_bpt + 1]
+    P_rad_upp = P_rad_upp[..., start_index_bpt:end_index_bpt + 1]
+    time_bpt = time_bpt[..., start_index_bpt:end_index_bpt + 1]
 
     print('Time selection done')
 
 
-    # MEDIAN FILTERING
+    # SAMPLING RATES
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Evaluate sampling rate for median filter function (sampling rates in the
+    # Evaluate sampling rate for subsequent downsampling (sampling rates in the
     # middle of a time trace are more reliable than averages)
     f_s_xpr = 1 / (time_xpr[int(time_xpr.size / 2) + 1] -
                    time_xpr[int(time_xpr.size / 2)])
@@ -221,25 +260,14 @@ for shot in [40365, 40366, 41157, 41158]:
                    time_fpg[int(time_fpg.size / 2)])
     f_s_ida = 1 / (time_ida[int(time_ida.size / 2) + 1] -
                    time_ida[int(time_ida.size / 2)])
-    f_s_iob = 1 / (time_iob[int(time_iob.size / 2) + 1] -
-                   time_iob[int(time_iob.size / 2)])
+    f_s_ioc = 1 / (time_ioc[int(time_ioc.size / 2) + 1] -
+                   time_ioc[int(time_ioc.size / 2)])
+    f_s_tot = 1 / (time_tot[int(time_tot.size / 2) + 1] -
+                   time_tot[int(time_tot.size / 2)])
+    f_s_bpt = 1 / (time_bpt[int(time_bpt.size / 2) + 1] -
+                   time_bpt[int(time_bpt.size / 2)])
 
-    # Apply median filters on IDA quantities
-    _, T_u = sgpr.median_filter(f_s_ida, time_window, time_ida, T_u)
-    time_ida, n_u = sgpr.median_filter(f_s_ida, time_window, time_ida, n_u)
-
-    # Apply median filters on IOB quantities
-    _, n_0_l = sgpr.median_filter(f_s_iob, time_window, time_iob, n_0_l)
-    _, n_0 = sgpr.median_filter(f_s_iob, time_window, time_iob, n_0)
-    time_iob, n_0_u = sgpr.median_filter(f_s_iob, time_window, time_iob, n_0_u)
-
-    # Re-evaluate sampling rate for filtered quantities
-    f_s_ida = 1 / (time_ida[int(time_ida.size / 2) + 1] -
-                   time_ida[int(time_ida.size / 2)])
-    f_s_iob = 1 / (time_iob[int(time_iob.size / 2) + 1] -
-                   time_iob[int(time_iob.size / 2)])
-
-    print('Median filters applied')
+    print('Sampling rates evaluated')
 
 
     # FLUX EXPANSION RATIO
@@ -259,6 +287,7 @@ for shot in [40365, 40366, 41157, 41158]:
                                                 coord_in='rho_pol')
         r_l95_int.append(r_l95_temp.flatten()[0])
         z_l95_int.append(z_l95_temp.flatten()[0])
+
     l95_intersection = np.array([r_l95_int, z_l95_int])
     xp_expansion = np.linalg.norm(l95_intersection - xp, axis=0).astype(np.double)
 
@@ -292,34 +321,39 @@ for shot in [40365, 40366, 41157, 41158]:
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Query for shortest time array. It will be used to evaluate sampling rate for
     # gaussian filters sigma computation and for resampling
-    time_wT = sgpr.shortest_array([time_xpr, time_fpg, time_ida, time_iob])
+    time_wT = sgpr.shortest_array([time_xpr, time_fpg, time_ida, time_ioc])
     f_s_wT = 1 / (time_wT[int(time_wT.size / 2) + 1] -
                   time_wT[int(time_wT.size / 2)])
 
     # Apply gaussian filter to XPR quantities
-    z_xpr_wT = ndm.gaussian_filter(z_xpr, f_s_xpr / (2 * f_s_wT))
     rho_xpr_wT = ndm.gaussian_filter(rho_xpr, f_s_xpr / (2 * f_s_wT))
+    rho_xpr_l_wT = ndm.gaussian_filter(rho_xpr_l, f_s_xpr / (2 * f_s_wT))
+    rho_xpr_u_wT = ndm.gaussian_filter(rho_xpr_u, f_s_xpr / (2 * f_s_wT))
 
     # Apply gaussian filter to FPG quantities
     R_0_wT = ndm.gaussian_filter(r_magax, f_s_fpg / (2 * f_s_wT))
     a_wT = ndm.gaussian_filter(a, f_s_fpg / (2 * f_s_wT))
     q_95_wT = ndm.gaussian_filter(q_95, f_s_fpg / (2 * f_s_wT))
-    z_xp_wT = ndm.gaussian_filter(z_xp, f_s_fpg / (2 * f_s_wT))
     f_exp_wT = ndm.gaussian_filter(f_exp, f_s_fpg / (2 * f_s_wT))
 
     # Apply gaussian filter to IDA quantities
     T_u_wT = ndm.gaussian_filter(T_u, f_s_ida / (2 * f_s_wT))
+    T_u_low_wT = ndm.gaussian_filter(T_u_low, f_s_ida / (2 * f_s_wT))
+    T_u_upp_wT = ndm.gaussian_filter(T_u_upp, f_s_ida / (2 * f_s_wT))
     n_u_wT = ndm.gaussian_filter(n_u, f_s_ida / (2 * f_s_wT))
+    n_u_low_wT = ndm.gaussian_filter(n_u_low, f_s_ida / (2 * f_s_wT))
+    n_u_upp_wT = ndm.gaussian_filter(n_u_upp, f_s_ida / (2 * f_s_wT))
 
     # Apply gaussian filter to IOB quantities
-    n_0_lwT = ndm.gaussian_filter(n_0_l, f_s_iob / (2 * f_s_wT))
-    n_0_wT = ndm.gaussian_filter(n_0, f_s_iob / (2 * f_s_wT))
-    n_0_uwT = ndm.gaussian_filter(n_0_u, f_s_iob / (2 * f_s_wT))
+    n_0_wT = ndm.gaussian_filter(n_0, f_s_ioc / (2 * f_s_wT))
+    n_0_low_wT = ndm.gaussian_filter(n_0_low, f_s_ioc / (2 * f_s_wT))
+    n_0_upp_wT = ndm.gaussian_filter(n_0_upp, f_s_ioc / (2 * f_s_wT))
 
     # Re-slice XPR quantities
     xpr_indexes_wT, _ = sgpr.find_nearest_multiple_index(time_xpr, time_wT)
-    z_xpr_wT = z_xpr_wT[xpr_indexes_wT]
     rho_xpr_wT = rho_xpr_wT[xpr_indexes_wT]
+    rho_xpr_l_wT = rho_xpr_l_wT[xpr_indexes_wT]
+    rho_xpr_u_wT = rho_xpr_u_wT[xpr_indexes_wT]
     time_xpr_wT = time_xpr[xpr_indexes_wT]
 
     # Re-slice FPG quantities
@@ -327,40 +361,39 @@ for shot in [40365, 40366, 41157, 41158]:
     R_0_wT = R_0_wT[fpg_indexes_wT]
     a_wT = a_wT[fpg_indexes_wT]
     q_95_wT = q_95_wT[fpg_indexes_wT]
-    z_xp_wT = z_xp_wT[fpg_indexes_wT]
     f_exp_wT = f_exp_wT[fpg_indexes_wT]
     time_fpg_wT = time_fpg[fpg_indexes_wT]
 
     # Re-slice IDA quantities
     ida_indexes_wT, _ = sgpr.find_nearest_multiple_index(time_ida, time_wT)
     T_u_wT = T_u_wT[ida_indexes_wT]
+    T_u_low_wT = T_u_low_wT[ida_indexes_wT]
+    T_u_upp_wT = T_u_upp_wT[ida_indexes_wT]
     n_u_wT = n_u_wT[ida_indexes_wT]
+    n_u_low_wT = n_u_low_wT[ida_indexes_wT]
+    n_u_upp_wT = n_u_upp_wT[ida_indexes_wT]
     time_ida_wT = time_ida[ida_indexes_wT]
 
     # Re-slice IOB quantities
-    iob_indexes_wT, _ = sgpr.find_nearest_multiple_index(time_iob, time_wT)
-    n_0_lwT = n_0_lwT[iob_indexes_wT]
-    n_0_wT = n_0_wT[iob_indexes_wT]
-    n_0_uwT = n_0_uwT[iob_indexes_wT]
-    time_iob_wT = time_iob[iob_indexes_wT]
+    ioc_indexes_wT, _ = sgpr.find_nearest_multiple_index(time_ioc, time_wT)
+    n_0_wT = n_0_wT[ioc_indexes_wT]
+    n_0_low_wT = n_0_low_wT[ioc_indexes_wT]
+    n_0_upp_wT = n_0_upp_wT[ioc_indexes_wT]
+    time_ioc_wT = time_ioc[ioc_indexes_wT]
 
     print('Downsampling executed')
 
 
     # EVALUATION OF X_A WITH T_u
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    X_A_lwT = R_0_wT**2 * q_95_wT**2 * f_exp_wT * n_u_wT * \
-                n_0_lwT / (a_wT * T_u_wT**(5/2))
     X_A_wT = R_0_wT**2 * q_95_wT**2 * f_exp_wT * n_u_wT * \
-                n_0_wT / (a_wT * T_u_wT**(5/2))
-    X_A_uwT = R_0_wT**2 * q_95_wT**2 * f_exp_wT * n_u_wT * \
-                n_0_uwT / (a_wT * T_u_wT**(5/2))
+            n_0_wT / (a_wT * T_u_wT**(5/2))
+    X_A_low_wT = R_0_wT**2 * q_95_wT**2 * f_exp_wT * n_u_low_wT * \
+            n_0_low_wT / (a_wT * T_u_upp_wT**(5/2))
+    X_A_upp_wT = R_0_wT**2 * q_95_wT**2 * f_exp_wT * n_u_upp_wT * \
+            n_0_upp_wT / (a_wT * T_u_low_wT**(5/2))
 
     print('X_A evaluated with T_u')
-
-    # EVALUATION OF XPR RELATIVE HEIGHT WITH T_u DOWNSAMPLING
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    z_xpr_rel_wT = z_xpr_wT - z_xp_wT
 
 
     # SLICING ONLY FOR COLD CORE XPR PHASES
@@ -371,9 +404,9 @@ for shot in [40365, 40366, 41157, 41158]:
     cld_XA_wT_idx = np.array([idx for start, end in zip(cld_start_XA_wT_idx,
                                                         cld_end_XA_wT_idx) for idx
                               in range(start, end + 1)])
-    cld_X_A_lwT = X_A_lwT[cld_XA_wT_idx]
+    cld_X_A_lwT = X_A_low_wT[cld_XA_wT_idx]
     cld_X_A_wT = X_A_wT[cld_XA_wT_idx]
-    cld_X_A_uwT = X_A_uwT[cld_XA_wT_idx]
+    cld_X_A_uwT = X_A_upp_wT[cld_XA_wT_idx]
 
     # Slice xpr
     cld_start_xpr_wT_idx, _ = sgpr.find_nearest_multiple_index(time_xpr_wT,
@@ -383,13 +416,16 @@ for shot in [40365, 40366, 41157, 41158]:
     cld_xpr_wT_idx = np.array([idx for start, end in zip(cld_start_xpr_wT_idx,
                                                          cld_end_xpr_wT_idx)
                                for idx in range(start, end + 1)])
-    cld_z_xpr_rel_wT = z_xpr_rel_wT[cld_xpr_wT_idx]
     cld_rho_xpr_wT = rho_xpr_wT[cld_xpr_wT_idx]
+    cld_rho_xpr_l_wT = rho_xpr_l_wT[cld_xpr_wT_idx]
+    cld_rho_xpr_u_wT = rho_xpr_u_wT[cld_xpr_wT_idx]
 
     # PLOT
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ax1.errorbar(cld_X_A_wT, 1-cld_rho_xpr_wT, xerr=[cld_X_A_wT - cld_X_A_lwT,
                                                      cld_X_A_uwT - cld_X_A_wT],
+                 yerr=[cld_rho_xpr_u_wT-cld_rho_xpr_wT,
+                       cld_rho_xpr_wT-cld_rho_xpr_l_wT],
                  fmt='o', label=f'{shot}')
 
 
